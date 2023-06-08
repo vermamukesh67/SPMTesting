2f194386e8398734a39b55ea8244123d7bde69c4
verma mukesh <vermamukesh67@gmail.com>
2023-06-08 19:13:01 +0800

    add package

--- BB/Package.swift
+++ BB/Package.swift
@@ -4,48 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "AtlasDesign",
+    name: "BB",
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AtlasDesign",
-            targets: ["AtlasDesign"]),
+            name: "BB",
+            targets: ["BB"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/RedAirship/SwiftTheme.git", branch: "master"),
-
-        // Use of Lottie animation may output warnings like the following:
-        // - nil host used in call to allowsSpecificHTTPSCertificateForHost
-        // - nil host used in call to allowsAnyHTTPSCertificateForHost:
-        //
-        // airbnb bug ref: https://github.com/airbnb/lottie-ios/issues/1256
-        // airbnb bug fix: https://github.com/airbnb/lottie-ios/pull/1524
-        // airbnb tagged versions: https://github.com/airbnb/lottie-ios/tags?after=3.4.2
-        // ocbc notified: https://rasxocbc.slack.com/archives/C0448L5TQP6/p1678692484570569
-        .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios.git", .exact("3.3.0")),
-
-        // This url allows AppCenter to build successfully
-        // We might need to investigate why AppCenter cannot fetch the packages from Gitlab
-        .package(url: "https://github.com/RedAirship/JTAppleCalendar.git", branch: "master")
-        // .package(url: "https://gitlab.redairship.tech/ocbc-frank/JTAppleCalendar.git", branch: "master")
+        .package(
+            name: "AtlasDesign",
+            url: "https://gitlab.redairship.tech/ocbc-frank/ocbc-components-ios.git",
+            branch: "dev"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "AtlasDesign",
+            name: "BB",
             dependencies: [
-                "SwiftTheme",
-                "Lottie",
-                "JTAppleCalendar"
-            ],
-            resources: [
-                .copy("Resources/Fonts/"),
-                .copy("Resources/Themes/")
+                "AtlasDesign",
             ]
-        ),
+        )
     ]
 )
